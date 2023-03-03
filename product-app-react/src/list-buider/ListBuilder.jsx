ffbc32baf7c3a215a0fc0a304cc1e58bd708d51b
jannat28-349 <jucse2.349@gmail.com>
2023-03-03 14:21:53 +0600

    Added validation

--- product-app-react/src/list-buider/ListBuilder.jsx
+++ product-app-react/src/list-buider/ListBuilder.jsx
@@ -1,16 +1,35 @@
 import { useState } from "react";
 import { ListInput } from "./ListInput";
 import { ListOutput } from "./ListOutput";
-import './ListBuilder.css';
+import "./ListBuilder.css";
 
 function ListBuilder() {
   const [newItem, setNewItem] = useState("");
   const [list, setList] = useState([]);
+  function validProduct() {
+    if (!newItem) {
+      alert("Product name can not be empty!");
+      return false;
+    }
+    if (
+      [...list].find((item) => {
+        return item === newItem;
+      })
+    ) {
+      alert("Duplicate product!");
+      return false;
+    }
+    if(newItem.length >= 10) {
+      alert("Product name is too large!");
+      return false;
+    }
+    return true;
+  }
   function handleOnChange(event) {
     setNewItem(event.target.value);
   }
   function handleOnClick() {
-    if (!newItem) {
+    if (!validProduct()) {
       return;
     }
     setList([...list, newItem]);
